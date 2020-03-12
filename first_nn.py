415227e94d064f1432791e017aa625323e3c0cef
Mikayla Campbell <hewantshiscar@Mikaylas-MacBook-Pro.local>
2020-03-12 16:16:06 -0700

    Sped it up

--- first_nn.py
+++ first_nn.py
@@ -33,8 +33,31 @@ def read_data(filename):
   y = data[:, -1]
   return ((x, y), varnames)
 
-def train_nn():
-	pass
+def train_nn(D, n, K, MaxIter):
+	# Set initial weights equal to zero
+	# Two layer network train
+	W = np.array(np.multiply(np.array(np.multiply([0], K)), np.size(D)))
+	v = np.array(np.multiply([0], K))
+	a = np.array(np.multiply([0], K))
+	h = np.array(np.multiply([0], K))
+	for i in range(MaxIter):
+		G = np.array(np.multiply(np.array(np.multiply([0], K)), np.size(D)))
+		g = np.array(np.multiply([0], K))
+		for x,y in D:
+			for j in range(1, K):
+				x2 = np.divide(np.dot(w[i], x), np.multiply(np.dot(w[i], x)), x)
+				a[i] = np.dot(W[i], x2)
+				h[i] = np.tanh(a[i])
+			y2 = np.dot(v, h)
+			e = np.subtract(y, y2)
+			g = np.subtract(g, np.multiply(e, h))
+			for j in range(1, k):
+				G[i] = np.subtract(g[i], np.multiply(e, np.multiply(np.multiply(v[i],
+					np.subtract(1, np.square(np.tanh(a[i])))), x)))
+		W = W - np.multiply(n, G)
+		v = np.subtract(v, np.multiply(n, g))
+
+	return W, v
 
 def predict_place():
 	pass
@@ -42,6 +65,10 @@ def predict_place():
 def main():
 	args = handle_args()
 	((train_x, train_y), varnames) = read_data(args.train)
+	hi, hey = train_nn(train_x, 0.01, train_y, 100)
+	print(hi)
+	print()
+	print(hey)
 
 if __name__ == "__main__":
 	main()
